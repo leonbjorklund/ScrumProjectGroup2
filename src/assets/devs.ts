f5b5b784499100412cb699d65b13b8e1396fdbae
ParhamInBinart <parham.berenjian@gmail.com>
2023-09-01 11:48:50 +0200

    list of devs and info

---
+++ src/assets/devs.ts
@@ -0,0 +1,37 @@
+export const devs = {
+  edvin: {
+    firstname: 'Edvin',
+    lastname: 'Djulic',
+    role: 'Front end developer',
+    about:
+      '“In my spare time I enjoy working out, watch sports and spend time with family and friends.”',
+    profileDesc: 'asdasd',
+    email: '123123',
+  },
+  leon: {
+    firstname: 'Leon',
+    lastname: 'Björklund',
+    role: 'UX-designer',
+    about:
+      '“I like creating and designing intuitive web-apps. I always strive  to create the best experience for the end user as possible.”',
+    profileDesc: 'asdasd',
+    email: '123123',
+  },
+  parham: {
+    firstname: 'Parham',
+    lastname: 'Berenjian',
+    role: 'Back end developer',
+    about: '“I enjoy nature walks and spending time with my family & firends.”',
+    profileDesc: 'asdasd',
+    email: '123123',
+  },
+  thomas: {
+    firstname: 'Thomas',
+    lastname: 'Ingvarsson',
+    role: 'Front end developer',
+    about:
+      '“My main hobbies are skateboarding and videogames. I allways like a challenge and to push myself to get better and more confident.”',
+    profileDesc: 'asdasd',
+    email: '123123',
+  },
+};
