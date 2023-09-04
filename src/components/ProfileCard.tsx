54e2b5768fd904c15dedd29115ab951279e2d63e
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-09-04 15:20:30 +0200

    Foxed so it scrolls to top when click teammember
    Co-authored-by: Parham Berenjian <ParhamInBinary@users.noreply.github.com>

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,4 +1,5 @@
 import { Box, SxProps, Theme, Typography } from '@mui/material';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { devs } from '../assets/devs';
 import NotFound from '../components/NotFound.tsx';
@@ -7,6 +8,12 @@ export default function ProfileCard() {
   const params = useParams();
 
   const dev = devs.find(dev => dev.firstname === params.firstname);
+
+  useEffect(() => {
+    if (!window.location.hash) {
+      window.scrollTo(0, 0);
+    }
+  }, []);
 
   if (!dev) {
     return <NotFound />;
@@ -25,16 +32,14 @@ export default function ProfileCard() {
           {dev.role}
         </Typography>
         <Box sx={DevCompBoxStyleSX}>
-        {dev.comp.map(comp => (
-          <Typography sx={DevCompStyleSX} variant='h1'>
-            {comp}
-          </Typography>
-        ))}
+          {dev.comp.map((comp, index) => (
+            <Typography sx={DevCompStyleSX} variant='h1' key={index}>
+              {comp}
+            </Typography>
+          ))}
         </Box>
         <Box sx={DevDescStyleSX}>
-        <Typography variant='body1'>
-          {dev.profileDesc}
-        </Typography>
+          <Typography variant='body1'>{dev.profileDesc}</Typography>
         </Box>
       </Box>
     </Box>
@@ -59,17 +64,16 @@ const DevCardStyleSX: SxProps<Theme> = theme => ({
 });
 const DevImgStyleSX: SxProps<Theme> = theme => ({
   '& img': {
-      height: '170px',
-    },
+    height: '170px',
+  },
   [theme.breakpoints.down('md')]: {
     '& img': {
-        height: '120px',
-      },
+      height: '120px',
+    },
   },
   [theme.breakpoints.down('sm')]: {},
 });
 const DevPresentationStyleSX: SxProps<Theme> = theme => ({
-
   [theme.breakpoints.down('md')]: {},
   [theme.breakpoints.down('sm')]: {
     display: 'flex',
@@ -105,14 +109,14 @@ const DevCompStyleSX: SxProps<Theme> = theme => ({
   fontSize: '18px',
   paddingTop: '.5rem',
   margin: '.2rem .5rem',
-  [theme.breakpoints.down('md')]: { 
+  [theme.breakpoints.down('md')]: {
     fontSize: '12px',
   },
   [theme.breakpoints.down('sm')]: {},
 });
 const DevDescStyleSX: SxProps<Theme> = theme => ({
   fontSize: '16px',
-    width: '70%',
+  width: '70%',
   paddingTop: '2rem',
   [theme.breakpoints.down('md')]: {
     width: '90%',
