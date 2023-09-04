b9ae02e6cceab1ea918672e8d697696cc40fa645
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-09-04 14:38:45 +0200

    added attributes to dev objects Co-authored-by: Parham Berenjian

--- src/assets/devs.ts
+++ src/assets/devs.ts
@@ -5,6 +5,7 @@ export interface Dev {
   firstname: string;
   lastname: string;
   role: string;
+  comp: string[];
   about: string;
   profileDesc: string;
   email: string;
@@ -16,6 +17,7 @@ export const devs: Dev[] = [
     firstname: 'Leon',
     lastname: 'Björklund',
     role: 'UX-designer',
+    comp: ['React',''],
     about:
       '“I like creating and designing intuitive web-apps. I always strive  to create the best experience for the end user as possible.”',
     profileDesc: 'asdasd',
@@ -26,6 +28,7 @@ export const devs: Dev[] = [
     firstname: 'Thomas',
     lastname: 'Ingvarsson',
     role: 'Front end developer',
+    comp: ['React','TS', 'EXPRESS', 'HTML', 'CSS', 'Node.js'],
     about:
       '“My main hobbies are skateboarding and videogames. I allways like a challenge and to push myself to get better and more confident.”',
     profileDesc: 'asdasd',
@@ -36,6 +39,7 @@ export const devs: Dev[] = [
     firstname: 'Parham',
     lastname: 'Berenjian',
     role: 'Back end developer',
+    comp: ['React', 'JS', 'Node.js', 'MongoDB', 'MYSQL'],
     about: '“I enjoy nature walks and spending time with my family & firends.”',
     profileDesc: 'With a combination of passion and education, my expertice in Back end development has a sturdy foundation and solid structure. I love learning new ways to improve both myself and my code.',
     email: '123123',
@@ -45,6 +49,7 @@ export const devs: Dev[] = [
     firstname: 'Edvin',
     lastname: 'Djulic',
     role: 'Front end developer',
+    comp: ['React',''],
     about:
       '“In my spare time I enjoy working out, watch sports and spend time with family and friends.”',
     profileDesc: 'asdasd',
