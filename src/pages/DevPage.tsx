1cb61b12a4dfece70db30bd95375177cc38f87d0
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-09-04 13:43:07 +0200

    Added structure on page
    Co-authored-by: Parham Berenjian <ParhamInBinary@users.noreply.github.com>
    Co-authored-by: Edvindjulic <Edvindjulic@users.noreply.github.com>

--- src/pages/DevPage.tsx
+++ src/pages/DevPage.tsx
@@ -1,7 +1,16 @@
+import { Box } from '@mui/system';
+import ContactForm from '../components/ContactForm';
+import Developers from '../components/Developers';
+import Portfolio from '../components/Portfolio';
+import ProfileCard from '../components/ProfileCard';
+
 export default function DevPage() {
   return (
-    <div>
-      <h1>Developers</h1>
-    </div>
+    <Box>
+      <ProfileCard />
+      <Portfolio />
+      <ContactForm />
+      <Developers />
+    </Box>
   );
 }
