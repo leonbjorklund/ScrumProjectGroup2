30ea828a0cfe3365c25bc24abce030e14442c4ed
Thomas ingvarsson <105804552+GunnarIng@users.noreply.github.com>
2023-09-05 21:52:07 +0200

    useLocation shows corresponding name as Title

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 // import { ThemeProvider } from '@emotion/react';
 import { ThemeProvider } from '@emotion/react';
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import './App.css';
 import { theme } from './Theme';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
 export default function App() {
+
+  // Change the title of the page based on the current route
+  const location = useLocation();
+  const developers: Record<string, string> = {
+    Parham: 'Developer - Parham',
+    Thomas: 'Developer - Thomas',
+    Edvin: 'Developer - Edvin',
+    Leon: 'Developer - Leon',
+  };
+  const title: string = developers[location.pathname.split('/')[2]] || 'FifthElement';
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
   return (
     <ThemeProvider theme={theme}>
       <>
