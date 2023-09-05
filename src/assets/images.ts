4a2d250cec8d7f1d624915c6b2adfb84aa1dd548
Leon Björklund <leon.bjorklund@gmail.com>
2023-09-05 15:22:02 +0200

    added Leon & Thomas projects, and tooltip Co-authored-by: Thomas Ingvarsson <thomas.ingvarsson@medieinstitutet.se>

--- src/assets/images.ts
+++ src/assets/images.ts
@@ -27,32 +27,21 @@ export const images = {
     'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264604854-facab66c-f738-47f4-9c31-c4e4d017a536.png',
 };
 
-// # Header Logo IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264602647-61a3e97d-d470-4d77-b60c-703e46d76d42.png
+export const thomasProjectImages = {
+  gents:
+    'https://github-production-user-asset-6210df.s3.amazonaws.com/105804552/265694106-2b8eb73d-ed45-46cb-8f41-5f21bef33275.png',
+  moon: 'https://github-production-user-asset-6210df.s3.amazonaws.com/105804552/265694901-3a217b76-01f5-4ad8-a000-4a0ea16ad54f.png',
+  apple:
+    'https://github-production-user-asset-6210df.s3.amazonaws.com/105804552/265695088-556fbb16-9b92-4319-bde3-f32157ad0089.png',
+  church:
+    'https://github-production-user-asset-6210df.s3.amazonaws.com/105804552/265695094-b6750a6b-d071-4c55-b37d-04d53e5bb62d.png',
+  quiz: 'https://github-production-user-asset-6210df.s3.amazonaws.com/105804552/265695098-b78b544d-8ed7-413a-b134-58b12bf0e110.png',
+  landlord:
+    'https://github-production-user-asset-6210df.s3.amazonaws.com/105804552/265695100-0fed95a5-ce73-4a56-b094-79ef9c0bdafb.png',
+};
 
-// # Hero IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264602286-1eed5533-3b5a-48d0-998f-372a34762772.png
+export const leonProjectsImages = {};
 
-// # Who we are IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264602797-07aeb851-4c9a-49d0-870d-030f3c1c08af.png
+export const parhamProjectsImages = {};
 
-// # Company services IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264352924-8c798236-c721-4edb-b8e3-2c57b2f427fd.png
-
-// # Leon IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264603124-02b6e38a-96c8-4c3d-a838-ce011da1fb7a.png
-
-// # Thomas IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264603409-bb69e7d3-916e-4e40-96fd-4c4cb84a4aef.png
-
-// # Parham IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264603524-6af3022e-8e4d-440d-8d92-fb895d841abd.png
-
-// # Edvin IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264603631-4202cc7c-d6ba-4d00-8e1d-c7f601c2cd86.png
-
-// # Contact form IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264602128-f09ed321-6fe6-44da-b29f-44d34a1a6f1a.png
-
-// # StarBucks IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264603759-474899bd-d095-4824-842f-539ff580ca12.png
-
-// # SwedBank IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264603770-e7b1df47-866a-4420-8d22-5ca3f507783f.png
-
-// # SvD IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264603785-03021ce9-aee7-4db5-b0f7-26a09854f89e.png
-
-// # Disney IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264603828-dcaaf3b8-afe6-4c3c-b450-dae4cc51294a.png
-
-// # KIA IMG: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264604731-f26d4282-0463-42e2-bae3-a21e69947cb1.png
-
-// # FooterLogo: https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264604854-facab66c-f738-47f4-9c31-c4e4d017a536.png
+export const edvinProjectsImages = {};
--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -1,3 +1,5 @@
+import { thomasProjectImages } from './images';
+
 export interface Project {
   name: string;
   img: string;
@@ -8,92 +10,92 @@ export interface Project {
 
 export const leonProjects: Project[] = [
   {
-    name: 'Leon project 1',
+    name: 'InfiTrak',
+    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265699194-be62ff37-e191-4995-990a-75c53f3919cb.png',
+    description:
+      'Created a fictional company that sells tracking devices, built with React, TypeScript, and Chakra UI. Features a fully functional shopping cart and checkout.',
+    githubLink: 'https://github.com/leonbjorklund/infitrak',
+    demoLink: 'https://leonbjorklund.github.io/infitrak/',
+  },
+  {
+    name: 'Bubble Tea Webshop',
+    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265699188-bae03cfe-99ab-40e1-868b-a2edddc22ab9.png',
+    description:
+      'A webshop built with React, TypeScript, and Chakra UI, allows customers to order and securely checkout. Admins can dynamically add or edit products.',
+    githubLink: 'https://github.com/leonbjorklund/bubbletea-webshop',
+    demoLink: 'https://bobabliss.netlify.app/',
+  },
+  {
+    name: 'Save Gothenburg Game',
+    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265699195-ccff7426-2763-4730-8980-b14d24d6d8e3.png',
+    description:
+      'A group project where we built a simple, original game created using TypeScript with p5, a JS-library for animations etc.',
+    githubLink: 'https://github.com/FED22GBGTeam1/GBGspel',
+    demoLink: 'https://save-gothenburg.netlify.app/',
+  },
+  {
+    name: 'Whispr',
+    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265699199-de2eff55-7db5-49cb-aa48-b5719c898da2.png',
+    description:
+      'My first attempt at a React fullstack project using Firebase for backend & hosting, enabling users to create and like text posts in a real-time environment.',
+    githubLink: 'https://github.com/leonbjorklund/whispr',
+    demoLink: 'https://whispr-e3191.web.app/',
+  },
+  {
+    name: 'Calendar App',
     img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265485605-0abcad9a-978a-47d9-8971-158963fcce92.png',
     description:
-      'Leon project description 1 LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum LoremIpsum',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
+      'A calendar built with vanilla JavaScript, allows users to create to-dos that are placed on specific dates. Used Cypress end to end testing.',
+    githubLink: 'https://github.com/leonbjorklund/JS-calendar',
+    demoLink: 'https://leonbjorklund.github.io/JS-calendar/',
   },
   {
-    name: 'Leon project 2',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265485605-0abcad9a-978a-47d9-8971-158963fcce92.png',
-    description: 'Leon project description 2',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
-  },
-  {
-    name: 'Leon project 3',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265485605-0abcad9a-978a-47d9-8971-158963fcce92.png',
-    description: 'Leon project description 3',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
-  },
-  {
-    name: 'Leon project 4',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265485605-0abcad9a-978a-47d9-8971-158963fcce92.png',
-    description: 'Leon project description 4',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
-  },
-  {
-    name: 'Leon project 5',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265485605-0abcad9a-978a-47d9-8971-158963fcce92.png',
-    description: 'Leon project description 5',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
-  },
-  {
-    name: 'Leon project 6',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265485605-0abcad9a-978a-47d9-8971-158963fcce92.png',
-    description: 'Leon project description 6',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
+    name: 'CraigsList',
+    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/265699192-e1948c80-aeff-440c-9754-fa9b451905fc.png',
+    description:
+      'Our craigslist redesign features a cleaner, more minimalistic look, making it easier for users to navigate the site with less distractions.',
+    githubLink: 'https://github.com/leonbjorklund/craigslist-redesign',
+    demoLink: 'https://leonbjorklund.github.io/craigslist-redesign/',
   },
 ];
 
 export const thomasProjects: Project[] = [
   {
-    name: 'Thomas project 1',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264663257-d2457ad0-343b-46c3-9672-123f566fe778.png',
-    description: 'Thomas project description 1',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
+    name: 'Clone Apple homepage',
+    img: thomasProjectImages.apple,
+    description:
+      'Made the startpage of apple.com from sratch with my classmate Simon Bengtsson. Html & Css used.',
+    githubLink: 'https://github.com/GunnarIng/Apple-frontpage-copy',
+    demoLink: 'https://gunnaring.github.io/Apple-frontpage-copy/',
   },
   {
-    name: 'Thomas project 2',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264663257-d2457ad0-343b-46c3-9672-123f566fe778.png',
-    description: 'Thomas project description 2',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
+    name: 'Small Church Music',
+    img: thomasProjectImages.church,
+    description: 'Project was to take an old webpage and modernise it. Using HTML & CSS only',
+    githubLink: 'href="https://gunnaring.github.io/Apple-frontpage-copy/',
+    demoLink: 'href="https://gunnaring.github.io/Inl-mning-2-Old-website/',
   },
   {
-    name: 'Thomas project 3',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264663257-d2457ad0-343b-46c3-9672-123f566fe778.png',
-    description: 'Thomas project description 3',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
+    name: 'E-commerce website',
+    img: thomasProjectImages.gents,
+    description:
+      'It has a working cart and checkout. You can modify orders, fill out a checkout form to place them, and access an admin user to manage products. Uses Yup for validation.',
+    githubLink: 'https://github.com/GunnarIng/react-webshop',
+    demoLink: 'https://gentshat.netlify.app/',
   },
   {
-    name: 'Thomas project 4',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264663257-d2457ad0-343b-46c3-9672-123f566fe778.png',
-    description: 'Thomas project description 4',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
+    name: 'Moon Mission',
+    img: thomasProjectImages.moon,
+    description: 'Space game using javascript and p5 library',
+    githubLink: 'https://github.com/GunnarIng/moon-mission',
+    demoLink: 'https://lysmac.github.io/moon-mission/',
   },
   {
-    name: 'Thomas project 5',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264663257-d2457ad0-343b-46c3-9672-123f566fe778.png',
-    description: 'Thomas project description 5',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
-  },
-  {
-    name: 'Thomas project 6',
-    img: 'https://github-production-user-asset-6210df.s3.amazonaws.com/45374596/264663257-d2457ad0-343b-46c3-9672-123f566fe778.png',
-    description: 'Thomas project description 6',
-    githubLink: 'https://theuselessweb.com/',
-    demoLink: 'https://theuselessweb.com/',
+    name: 'The quizie-quiz',
+    img: thomasProjectImages.quiz,
+    description: 'A quiz game built with React TS and JSS using an API for the whole game.',
+    githubLink: 'https://github.com/GunnarIng/Quiz-TLS',
+    demoLink: 'href="https://quiziequiz.netlify.app/',
   },
 ];
 
